b4955085358f3cfa6affb6ddf8e0ac1dcd218c26
dwchoi0610 <dwchoi0610@gmail.com>
2023-06-06 19:41:31 +0900

    compiler implement finish

--- main.py
+++ main.py
@@ -34,114 +34,151 @@ def get_code(code: str):
 
 # tokenize
 def tokenizer(sequence: str):
-    tokens = list(sequence.split())
+    tokens = ["|"] + list(sequence.split()) + ["$"]
     return tokens
+
 
 # Parser
 class Parser(object):
     def __init__(self, table, tokens, cfg):
-        self.table = table
-        self.tokens = tokenizer(tokens)
-        self.cur = 0
-        self.state = 0
-        self.cfg_table = cfg
-        self.cur_stack = []
+        self.table = table # parsing table
+        self.tokens = tokenizer(tokens) # tokens list
+        self.splitter = 0 # splitter |
+        self.state = 0 # 이동할 state
+        self.cur = 0 # left viable prefix 할 때, 사용할 cur
+        self.cur_stack = [0]
+        self.cfg_table = cfg # cfg 들이 담김
+        # self.state_stack = [0] # state 들의 stack # 0은 초기값
 
     # parse
     def parse(self):
         print(self.table)
-        self.cur_stack.append(0)
         while True:
-            next_input = self.tokens[self.cur]
-            next_rule = self.table[self.state][next_input]
-            print("cursor:", self.cur, " next_input:", next_input)
-            print("tokens:", self.tokens)
-            print("Let's parse. state:", self.state, "cfg:", next_rule)
-            if not self.check_rule(next_rule):
+            print("\n===============================================================================================\n")
+            print("-----------------------start left viable prefix check-----------------------")
+            if self.check_viable_prefix(): # left check
+                target = self.tokens[self.splitter + 1]
+                next_rule = self.table[self.state][target]
+            else:
+                print("error")
+            print("-----------------------left viable prefix check finish-----------------------")
+
+            next_rule = self.table[self.state][self.tokens[self.splitter+1]]
+            print()
+            print("-----------------------parsing start-----------------------")
+            print("Parsing rule:", next_rule)
+            if not self.check_rule(next_rule, left=False): # 실제 parse
                 break
-            print("after. cursor:", self.cur)
-            print("tokens:", self.tokens)
-            print("state:", self.state)
-            print("------------------------------")
+            print("After parsing:", self.tokens)
+            print("-----------------------parsing finish-----------------------")
+
+
+    # viable_prefix check
+    def check_viable_prefix(self):
+        # 현재 state -> rule check해서 행동
+        self.cur_stack = [0]
+        self.state = 0
+        if self.splitter == 0:
+            return True
+        else:
+            self.cur = 0
+        next_rule = self.table[self.state][self.tokens[self.cur]]
+        print("start cursor:", self.cur, "start state:", self.state, "start rule:", next_rule)
+        while self.check_rule(next_rule):
+            if self.cur == self.splitter: # viable prefix가 handle 맞음
+                print("final state: ", self.state)
+                return True
+            self.cur_stack.append(self.state)
+            next_rule = self.table[self.state][self.tokens[self.cur]]
+            print("current cursor:", self.cur, "current state:", self.state, "current rule:", next_rule)
+
+        print("\n\nViable prefix failed!")
+        return False
+
+
 
     # forward / 앞으로 나가기
     def forward(self):
-        self.cur += 1
+        self.tokens[self.splitter], self.tokens[self.splitter + 1] \
+            = self.tokens[self.splitter + 1], self.tokens[self.splitter]
+        self.splitter += 1
+
 
     # table 속 rule 확인
-    def check_rule(self, rule):
+    def check_rule(self, rule, left=True):
         if isinstance(rule, int):
             # goto
-            print("goto")
             self.state = int(rule)
-            self.cur_stack.append(self.state)
+            if left:
+                self.cur += 1
+            else:
+                print("rule:", rule)
+                self.forward()
             return True
 
         elif isinstance(rule, str):
             # shift
             if rule[0] == 's':
-                self.shift(rule)
+                self.shift(rule, left)
                 return True
             elif rule[0] == 'r':
-                self.reduce(rule)
+                self.reduce(rule, left)
                 return True
             elif rule == 'acc':
+                print("-----------------------parsing finish-----------------------")
+                print("\n\nPROGRAM FINISHED!")
                 return False
-            else: # error
+            else: # exception
                 return False
 
-    def shift(self, rule):
-        # shift
-        self.state = int(rule[1:])
-        self.cur_stack.append(self.state)
-        print("cur_stack:", self.cur_stack)
-        self.forward()
+    # shift
+    def shift(self, rule, left):
+        # 전처리
+        next_state = int(rule[1:])
+        # action
+        # state shift
+        self.state = next_state
+        if left:
+            self.cur += 1
+        else:
+            self.forward()
 
-        # error_check
+        # exception
         pass
 
-    def reduce(self, rule):
+    def reduce(self, rule, left):
         # reduce
+        if left:
+            return False
+
         cfg = self.cfg_table[int(rule[1:])]
         print("reduce rule: ", cfg)
         lhs, rhs = cfg
         rhs_tokens = list(rhs.split())
         reduce_len = len(rhs_tokens)
+
+        # epsilon reduce
         if rhs_tokens == ["''"]:
-            self.tokens.insert(self.cur, lhs)
-            self.forward()
+            self.tokens.insert(self.splitter, lhs)
+            self.splitter += 1
 
         else:
             if reduce_len == 1:
-                self.tokens[self.cur-1] = lhs
+                self.tokens[self.splitter-1] = lhs
             elif reduce_len > 1:
-                self.tokens[self.cur-1] = lhs
-                del self.tokens[(self.cur-1)-(reduce_len-1):(self.cur-1)]
-                self.cur -= (reduce_len-1)
+                self.tokens[self.splitter-1] = lhs
+                del self.tokens[(self.splitter-1)-(reduce_len-1):(self.splitter-1)]
+                self.splitter -= (reduce_len-1)
 
             for _ in range(reduce_len):
                 self.cur_stack.pop()
 
-            self.state = self.cur_stack[-1]
-        print("cur_stack:", self.cur_stack)
-
-
-        # cur로부터 len만큼 앞에 없앰 (만약 0이다?, 그냥 자기만 바꿈.) (1이상? 자기 포함 앞에 바꿈)
-        # (cur바꿈)
-        # (cur 바꿈) (cur - reduce_len:cur까지 del해버림)
-        # cur을 cur - reduce_len으로 초기화
-
-        # del my_list[start:end]
-        # cur input이 그대로 들어감
-        # lhs -> rhs
-        # rhs를 다시 lhs로 바꿔야 함
-        # rhs가 여러개라면 어떻게...?
-
-        # error_check
         pass
 
 
-sequence = "vtype id lparen vtype id comma vtype id rparen lbrace return num semi rbrace"
+# sequence = "vtype id lparen vtype id comma vtype id rparen lbrace return num semi rbrace"
+# sequence = "class id lbrace vtype id assign num semi vtype id lparen vtype id comma vtype id rparen lbrace return num semi rbrace rbrace"
+sequence = "vtype id assign lparen num addsub num rparen multdiv num semi vtype id lparen vtype id rparen lbrace if lparen boolstr comp boolstr comp boolstr rparen lbrace id assign num semi rbrace return num semi rbrace"
 my_parser = Parser(call_table("table"), sequence, call_cfg("cfg"))
 print(my_parser.cfg_table)
 my_parser.parse()
--- tokenizer.py
+++
@@ -1,8 +0,0 @@
-class Tokenizer(object):
-    def __init__(self, sequence):
-        self.sequence = sequence
-        self.tokens = list()
-
-    def tokenize(self):
-        self.tokens = list(self.sequence.split())
-        return self.tokens
